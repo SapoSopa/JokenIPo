b3f92453f27d65fea6556edd332da8e41157e2e1
João Miguel Francisco de Souza <144076975+jmfs12@users.noreply.github.com>
2023-09-17 19:09:13 -0300

    Update audio.h

--- include/audio.h
+++ include/audio.h
@@ -6,11 +6,12 @@ void StartMusic(); // start playing a music that could be identified by a config
 void ChangeMusic(int musicIdx); // change music to another one
 void PauseMusic(); // pauses music
 
+void UpdateMusic();
 void PlaySoundIdx(int soundIdx); // play a sound that could be identified by a index
 void ChangeMusicVolume(float volume, int musicIdx); // change the music volume
 void ResumeMusic(int musicIDX);
-int LoadSoundFile(const char *fileName); // load sound and save in dynamic array of sounds
-int LoadMusicFile(const char *fileName); // load music and save in dynamic array of musics
+void LoadSoundFile(const char *fileName); // load sound and save in dynamic array of sounds
+void LoadMusicFile(const char *fileName); // load music and save in dynamic array of musics
 
 void UnloadAll_Audio(); // unload all sounds and musics and free memory
 // ---------------------------------------------------
