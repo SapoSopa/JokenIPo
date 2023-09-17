ea45b3939caa278e57665909c135c5aef0f6acdc
João Miguel Francisco de Souza <144076975+jmfs12@users.noreply.github.com>
2023-09-17 13:45:00 -0300

    Update resourcesIdx.h

--- include/resourcesIdx.h
+++ include/resourcesIdx.h
@@ -3,17 +3,36 @@
 
 // musics index
 
-#define Music_name_1 0
-#define Music_name_2 1
-#define Music_name_3 2
-#define Music_name_4 3
+#define music_main 0
+#define music_second 1
+#define music_map1 2
+#define music_map2 3
+#define music_map3 4
+#define music_map4 5
+#define music_map5 6
+#define music_map6 7
+#define music_map7 8
+#define music_map8 9
+#define music_map9 10
+#define music_map10 11
+#define music_map11 12
+#define music_end 13
 
 // sounds index
 
-#define Sound_name_1 0
-#define Sound_name_2 1
-#define Sound_name_3 2
-#define Sound_name_4 3
+#define sound_hit  0
+#define sound_walk 1
+#define sound_door 2
+#define sound_death 3
+#define sound_fire 4
+#define sound_scissors 5
+#define sound_paper 6
+#define sound_rock 7
+#define sound_gun 8
+#define sound_water 9
+#define sound_sponge 10
+#define sound_human 11
+#define sound_air 12
 
 // textures index
 
