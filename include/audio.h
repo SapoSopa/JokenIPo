016191232f60ae89a60376bf00a2980370c71169
João Miguel Francisco de Souza <144076975+jmfs12@users.noreply.github.com>
2023-09-17 19:31:51 -0300

    Update audio.h

--- include/audio.h
+++ include/audio.h
@@ -2,14 +2,17 @@
 #define AUDIO_H
 
 // declaracao das funcoes --------------------------
+int MusicTest(); // aux function
 void StartMusic(); // start playing a music that could be identified by a config file
 void ChangeMusic(int musicIdx); // change music to another one
 void PauseMusic(); // pauses music
 
 void UpdateMusic();
 void PlaySoundIdx(int soundIdx); // play a sound that could be identified by a index
-void ChangeMusicVolume(float volume, int musicIdx); // change the music volume
-void ResumeMusic(int musicIDX);
+// to change the master volume, use SetMasterVolume(float volume)
+void ChangeMusicVolume(float volume,); // change all musics volume
+void ChangeSoundEffectVolume(float volume); // change all sound effects volume
+void ResumeMusic(int musicIDX); // resume a paused music
 void LoadSoundFile(const char *fileName); // load sound and save in dynamic array of sounds
 void LoadMusicFile(const char *fileName); // load music and save in dynamic array of musics
 
