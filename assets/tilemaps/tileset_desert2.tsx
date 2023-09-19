560e7397aa54c632c7b34a1fdd52631f41af8f15
João Miguel Francisco de Souza <144076975+jmfs12@users.noreply.github.com>
2023-09-19 07:05:11 -0300

    Add files via upload

---
+++ assets/tilemaps/tileset_desert2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Pyramid with Door" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../Pixel Art Ancient Egypt/Pyramid with Door.png" width="256" height="256"/>
+</tileset>
