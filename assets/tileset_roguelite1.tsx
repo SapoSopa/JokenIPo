d572d7796d60977fd2e4fdc5bd1012adffd8390b
João Miguel Francisco de Souza <144076975+jmfs12@users.noreply.github.com>
2023-09-19 14:30:45 -0300

    Add files via upload

---
+++ assets/tileset_roguelite1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset x1" tilewidth="32" tileheight="32" tilecount="851" columns="37">
+ <image source="../OneDrive/Área de Trabalho/roguelite/tileset x1.png" width="1184" height="736"/>
+</tileset>
